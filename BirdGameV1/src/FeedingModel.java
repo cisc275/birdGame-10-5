7278092b101a7ad7fbdad5f029477320fac7ba88
bbugieda <brian.bugieda@gmail.com>
2019-04-03 21:48:31 -0400

    FeedingModel class and test file

---
+++ BirdGameV1/src/FeedingModel.java
@@ -0,0 +1,48 @@
+import org.junit.Test;
+
+public class FeedingModel extends Model {
+
+	public FeedingModel() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean detectCollisions() {
+		return false;
+	}
+	
+	public void updateLocationAndDirection() {
+		
+	}
+	
+	public void dive() {
+		
+	}
+	
+	public boolean isHoldingFish() {
+		return false;
+	}
+}
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+class FeedingModelTest {
+	@Test
+	public void testDetectCollisions() {
+		
+	}
+	
+	@Test
+	public void testUpdateLocationAndDirection() {
+		
+	}
+	
+	@Test
+	public void testDive() {
+		
+	}
+	
+	@Test
+	public void testIsHoldingFish() {
+		
+	}
+}
