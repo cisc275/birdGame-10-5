9b762299943b8e6dc9b9cb3f721ac7e5a4f15bbe
MaxLuu29 <datluu829@gmail.com>
2019-04-03 22:15:24 -0400

    side swiper model made

---
+++ BirdGameV1/src/SideSwiperModel.java
@@ -0,0 +1,90 @@
+import org.junit.jupiter.api.Test;
+
+public class SideSwiperModel extends Model {
+	
+	@Override
+	public void updateLocationAndDirection() 
+	{
+		
+	}
+	
+	public void updateBirdLocation()
+	{
+		
+	}
+	
+	public void updateObstacleLocation() 
+	{
+		
+	}
+	
+	@Override
+	public boolean detectCollisions()
+	{
+		return false;
+	}
+	
+	public void randomizeBlocks()
+	{
+		
+	}
+	
+	public void randomizeQuestion()
+	{
+		
+	}
+	
+	public void moveObstacles()
+	{
+		
+	}
+	
+}
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+class SideSwiperModelTest {
+	
+	@Test
+	public void testUpdateLocationAndDirection() 
+	{
+		
+	}
+	
+	@Test
+	public void testUpdateBirdLocation()
+	{
+		
+	}
+	
+	@Test
+	public void testUpdateObstacleLocation() 
+	{
+		
+	}
+	
+	@Test
+	public void testDetectCollisions()
+	{
+		
+	}
+	
+	@Test
+	public void testRandomizeBlocks()
+	{
+		
+	}
+	
+	@Test
+	public void testRandomizeQuestion()
+	{
+		
+	}
+	
+	@Test
+	public void testMoveObstacles()
+	{
+		
+	}
+}
+
