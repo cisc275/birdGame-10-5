e70294732a3f295b0a3f09f3d28cdfe41606616d
Ryan Emenheiser <ryane@udel.edu>
2019-04-04 20:37:36 -0400

    created MiniMapModel.java

---
+++ BirdGameV1/src/MiniMapModel.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Test;
+
+public class MiniMapModel extends Model {
+	
+	@Override
+	public void updateLocationAndDirection()	{
+		
+	}
+	
+	public void plotPoint()	{
+		
+	}
+}
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+class MiniMapModelTest {
+
+	@Test
+	public void testUpdateLocationAndDirection() {
+		
+	}
+	
+	@Test
+	public void testPlotPoint() {
+
+	}
+
+}
