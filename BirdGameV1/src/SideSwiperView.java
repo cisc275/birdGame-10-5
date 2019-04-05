53ebe22f929005d069390610f16eb5afa71f8300
Justin Havel <havelj@udel.edu>
2019-04-04 21:37:30 -0400

    Adding SideSwiperView.java class stub and tests

---
+++ BirdGameV1/src/SideSwiperView.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+
+public class SideSwiperView extends View{
+	
+	public void displayBlocks() {
+		
+	}
+	
+	@Override
+	public void drawBackground() {
+		
+	}
+	
+}
+
+
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+
+class SideSwiperViewTest {
+	
+	@Test
+	public void displayBlocks() {
+		
+	}
+	
+	@Test
+	public void drawBackground() {
+		
+	}
+}
