6fec3e9b2eadb920c4a4117b0adbf550613572bf
Justin Havel <havelj@udel.edu>
2019-04-04 21:45:18 -0400

    adding WhackAMoleView.java class stub and tests

---
+++ BirdGameV1/src/WhackAMoleView.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+public class WhackAMoleView extends View {
+	
+	@Override
+	public void drawBackground() {
+		
+	}
+	
+	public void displayObjects() {
+		
+	}
+	
+}
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+class WhackAMoleViewTest {
+
+	@Test
+	public void testDrawBackground() {
+		
+	}
+	
+	@Test
+	public void testDisplayObjects() {
+		
+	}
+
+}
