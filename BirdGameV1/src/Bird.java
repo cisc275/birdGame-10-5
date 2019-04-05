68551fe432261bf6b0853a8c00e0c5d644f5fb00
bbugieda <brian.bugieda@gmail.com>
2019-04-04 22:29:26 -0400

    Bird class and test; Osprey and NorthernHarrier classes

---
+++ BirdGameV1/src/Bird.java
@@ -0,0 +1,86 @@
+import org.junit.jupiter.api.Test;
+
+public class Bird {
+	private int xPosition;
+	private int YPosition;
+	private int health;
+	private String sprite;
+	
+	public Bird() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void moveLeft() {
+		
+	}
+	
+	public void moveRight() {
+		
+	}
+	
+	public void moveUp() {
+		
+	}
+	
+	public void moveDown() {
+		
+	}
+	
+	public int getxPosition() {
+		return xPosition;
+	}
+
+	public void setxPosition(int xPosition) {
+		this.xPosition = xPosition;
+	}
+
+	public int getYPosition() {
+		return YPosition;
+	}
+
+	public void setYPosition(int yPosition) {
+		YPosition = yPosition;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public String getSprite() {
+		return sprite;
+	}
+
+	public void setSprite(String sprite) {
+		this.sprite = sprite;
+	}
+
+}
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+
+class BirdTest {
+	@Test
+	public void testMoveLeft() {
+		
+	}
+	
+	@Test
+	public void testMoveRight() {
+		
+	}
+	
+	@Test
+	public void testMoveUp() {
+		
+	}
+	
+	@Test
+	public void testMoveDown() {
+		
+	}
+}
