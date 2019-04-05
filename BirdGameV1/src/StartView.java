b821c605572244b68a6756fbfce3737654bce3d0
bbugieda <brian.bugieda@gmail.com>
2019-04-04 21:05:43 -0400

    StartView class and test

---
+++ BirdGameV1/src/StartView.java
@@ -0,0 +1,56 @@
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("serial")
+public class StartView extends View {
+
+	private boolean tutorialClicked;
+	private boolean startClicked;
+	
+	public StartView() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void createBirdObject() {
+		
+	}
+	
+	@Override
+	public void drawBackground() {
+		
+	}
+	
+	public boolean getTutorialClicked() {
+		return this.tutorialClicked;
+	}
+	
+	public boolean getStartClicked() {
+		return this.startClicked;
+	}
+}
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+
+class StartViewTest {
+	
+	@Test
+	public void createBirdObject() {
+		
+	}
+	
+	@Test
+	public void drawBackground() {
+		
+	}
+	
+	@Test
+	public void testGetTutorialClicked() {
+		
+	}
+	
+	@Test
+	public void testGetStartClicked() {
+		
+	}
+	
+}
