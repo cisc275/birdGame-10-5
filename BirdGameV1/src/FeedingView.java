8f02725ab7e28ec647adbad965c9ddc8d7adf2b2
MaxLuu29 <datluu829@gmail.com>
2019-04-04 21:27:52 -0400

    FeedingView made

---
+++ BirdGameV1/src/FeedingView.java
@@ -0,0 +1,47 @@
+import java.awt.image.BufferedImage;
+
+import org.junit.jupiter.api.Test;
+
+public class FeedingView extends View {
+	
+	private BufferedImage[][] fishingAnimationArray;
+	
+	public void displayFish() 
+	{
+		
+	}
+	
+	@Override
+	public void drawBackground()
+	{
+		
+	}
+
+	public BufferedImage[][] getFishingAnimationArray() {
+		return fishingAnimationArray;
+	}
+
+	public void setFishingAnimationArray(BufferedImage[][] fishingAnimationArray) {
+		this.fishingAnimationArray = fishingAnimationArray;
+	}
+	
+	
+}
+
+//-----------------------------------------------------------------------------------------------------
+//JUnit Tests
+
+class FeedingViewTest {
+	
+	@Test
+	public void testDisplayFish()
+	{
+		
+	}
+	
+	@Test
+	public void testDrawBackground()
+	{
+		
+	}
+}
